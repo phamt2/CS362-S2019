7d2f359d0542e6741dfe678bababaf8511c4a2f6
Tom <phamt2@oregonstate.edu>
2019-05-11 00:37:19 -0700

    Adding randomquiz

---
+++ projects/phamt2/quiz/testme.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+    //Get each character in the ascII table starting at 32 excluding delete
+    
+    char randomChar = 32 + rand() % 94;
+
+    return randomChar;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+    //Generates random characters starting from lower case a ending with lower case z to decrease the number of combinations and sticks them into a static string array so we can actually return the string array and it was initialized to 5 so that the 6th letter call would be a null since it does not exist in a 5 letter string.
+
+    static char randomString[5];
+    for (int a = 0; a < 5; a++)
+    {
+        char randomLetter = 97 + rand() % 26 ;
+        randomString[a] = randomLetter;
+    };
+        
+    return randomString;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
